a6905bc48f6272ae87950f6cea739c6fd349cb2b
mika.urakawa <awakaru0122@gmail.com>
2023-03-06 22:55:29 +0900

    refactor: add class

--- src/sendThemeToSlack.ts
+++ src/sendThemeToSlack.ts
@@ -1,195 +1,221 @@
-/**
- * スプレッドシートからテーマを取得、ない場合は OpenAI でテーマを生成して、Slack に投稿する（日本語対応のみ）
- * @param sheetName スプレッドシート名
- * @param sheetId スプレッドシート ID
- * @param slackBotToken Slack の Bot Token
- * @param slackChannelId Slack の Channel ID
- * @param openAIAPIKey OpenAI の API Key
- * @param content Slack に投稿するメッセージ。文字列または Block Kit（テーマを表示したい箇所は`@topic`とする）
- */
-function postTopicToSlack(
-    sheetName: string,
-    sheetId: string,
-    slackBotToken: string,
-    slackChannelId: string,
-    openAIAPIKey: string,
-    content: {} | string,
-) {
-    // スプレッドシートの列を取得
-    const sheet = SpreadsheetApp.openById(sheetId).getSheetByName(sheetName);
-    const dateList = getDateList_(sheet);
+export class Spreadsheet {
+    sheet: GoogleAppsScript.Spreadsheet.Sheet
 
-    // 翌日の取得
-    const nextdayString = getNextDate_();
-    console.log(nextdayString)
-
-    // テーマを取得（スプレッドシートにあればそれを、なければOpenAIで生成する）
-    let theme = getTheme_(dateList, sheet, nextdayString, openAIAPIKey);
-    console.log(theme);
-
-    // Slackに投稿
-    postTextToSlack_(slackBotToken, slackChannelId, content, theme);
-}
-
-/**
- * スプレッドシートのシート情報を取得する
- */
-function getSheet_(sheetName: string): GoogleAppsScript.Spreadsheet.Sheet {
-    const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-    const sheet = spreadsheet.getSheetByName(sheetName);
-    return sheet;
-}
-
-/**
- * スプレッドシートから日付のリストを取得する
- */
-function getDateList_(sheet: GoogleAppsScript.Spreadsheet.Sheet): string[][] {
-    const lastRow = sheet.getLastRow();
-    const dateRange = sheet.getRange(2, 1, lastRow - 1);
-    return dateRange.getDisplayValues();
-}
-
-/**
- * 翌日の日付を yyyy/M/dd で取得する
- */
-function getNextDate_(): string {
-    let nextday = new Date();
-    console.log(nextday)
-    nextday.setDate(nextday.getDate() + 1);
-    nextday.setHours(0);
-    nextday.setMinutes(0);
-    nextday.setSeconds(0);
-    nextday.setMilliseconds(0);
-    return nextday.toLocaleDateString('ja-JA');
-}
-
-/**
- * 日付のリストを元にスプレッドシートからテーマを取得する。
- * スプレッドシートにテーマがないときは、OpenAI でテーマを生成する
- */
-function getTheme_(
-    dateList: string[][],
-    sheet: GoogleAppsScript.Spreadsheet.Sheet,
-    nextdayString: string,
-    openAIAPIKey: string
-): string {
-    let theme = "";
-    for (let i = 0; i < dateList.length; i++) {
-        if (dateList[i][0] == nextdayString) {
-            // 隣のセルの番号にあるテーマを取得する
-            const cell = `B${i + 2}`;
-            const specificDateRange = sheet.getRange(cell);
-            theme = specificDateRange.getDisplayValue();
-
-            // スプレッドシートにテーマがないときは、OpenAI でテーマを生成する
-            if (theme == "") {
-                theme = requestOpenAI_(openAIAPIKey);
-                writeThemeToSpreadsheet_(theme, sheet, cell)
-            }
-        }
-    }
-    return theme;
-}
-
-/**
- * OpenAI Text Completion API にリクエストして雑談テーマを生成する
- */
-function requestOpenAI_(apiKey: string): string {
-    const url = "https://api.openai.com/v1/completions";
-    const prompt = "あなたはChatbotで、陽気で話しかけやすい人です。\n以下の制約条件を厳密に守って、雑談テーマを1つ提案してください。\n制約条件\n* Chatbotは雑談テーマをできるだけ詳しく提案します。\n* 「おしゃべりマン」というbotです。\n* 「好きな〇〇」や「気になる〇〇」という形式で答えます。\n* 「おしゃべりマン」のあなただったらどう答えるかも教えてください。\n* 一人称は「私」です。\n* ジェンダー、家族、体、政治、宗教の話題は避けてください。";
-
-    const headers = {
-        "Authorization": `Bearer ${apiKey}`
-    }
-    const params: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
-        method: "post",
-        muteHttpExceptions: true,
-        contentType: "application/json",
-        headers: headers,
-        payload: JSON.stringify({
-            'model': 'text-davinci-003',
-            'max_tokens': 200,
-            'temperature': 0.8,
-            'top_p': 1.0,
-            'frequency_penalty': 1.0,
-            'presence_penalty': 1.0,
-            'prompt': prompt
-        })
+    constructor(sheetID: string, sheetName: string) {
+        this.sheet = SpreadsheetApp.openById(sheetID).getSheetByName(sheetName)
     }
 
-    const response = JSON.parse(UrlFetchApp.fetch(url, params).getContentText());
-    console.log(response);
-    let theme = response.choices[0].text.replace("\n", "");
-    return theme;
+    write(cell: string, content: string) {
+        let range = this.sheet.getRange(cell);
+        range.setValue(content);
+    }
 }
 
-/**
- * 生成したテーマをスプレッドシートに書き込む
- */
-function writeThemeToSpreadsheet_(theme: string, sheet: GoogleAppsScript.Spreadsheet.Sheet, cell: string) {
-    let range = sheet.getRange(cell);
-    range.setValue(theme);
-};
+export class SlackClient {
+    private token: string
 
-/**
- * Slackにテーマを投稿する
- */
-function postTextToSlack_(token: string, channel: string, content: {} | string, theme: string) {
-    if (theme == "") {
-        return
-    };
+    /**
+     * 
+     * @param token Slack の Bot Token
+     */
+    constructor(token: string) {
+        this.token = token
+    }
 
-    let message = {};
-    if (typeof content == "string") {
-        const text = generateNotifyText_(content, theme);
-        message = {
-            token: token,
-            text: text,
+    /**
+     * Slack の メッセージ投稿APIをリクエストする
+     * @param block Block Kit 
+     * @returns 
+     */
+    postMessage(channel: string, blocks: Object) {
+        const message = {
+            token: this.token,
+            blocks: blocks['blocks'],
             channel: channel,
         }
-    } else {
-        const blocks = generateNotifyBlockMessage_(content, theme);
-        message = {
-            token: token,
-            blocks: blocks['blocks'],
-            channel: channel,    
-        }
+
+        const postRequestUrl = "https://slack.com/api/chat.postMessage";
+
+        const messagePayload = JSON.stringify(message);
+        const messageOptions: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
+            method: "post",
+            contentType: "application/json",
+            payload: messagePayload,
+            headers: { "Authorization": `Bearer ${this.token}` }
+        };
+
+        UrlFetchApp.fetch(postRequestUrl, messageOptions);
     }
-    console.log(message);
-
-    const postRequestUrl = "https://slack.com/api/chat.postMessage";
-
-    const messagePayload = JSON.stringify(message);
-    const messageOptions: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
-        method: "post",
-        contentType: "application/json",
-        payload: messagePayload,
-        headers: { "Authorization": `Bearer ${token}` }
-    };
-
-    UrlFetchApp.fetch(postRequestUrl, messageOptions);
 }
 
-/**
- * Slack通知用のメッセージ作成
- */
-function generateNotifyText_(context: string, theme: string): string {
-    let keyString = "@topic";
-    if (context.includes(keyString)) {
-        return context.replace(keyString, theme);
-    }
-    return context;
-} 
+export class OpenAIClient {
+    private apiKey: string
 
-/**
- * Slack通知用のBlockメッセージ作成
- */
-function generateNotifyBlockMessage_(content: {}, theme: string) {
-    let jsonString = JSON.stringify(content);
-    let keyString = "@topic";
-    if (jsonString.includes(keyString)) {
-        let replacedString = jsonString.replace(keyString, theme);
-        return JSON.parse(replacedString);
+    /**
+     * 
+     * @param apiKey OpenAI の API Key
+     */
+    constructor(apiKey: string) {
+        this.apiKey = apiKey
     }
-    return content;
-};
+
+    /**
+     * Text Completion API をリクエストする
+     * @param prompt プロンプト
+     * @returns 
+     */
+    requestTextCompletion(prompt: string): any {
+        const url = "https://api.openai.com/v1/completions";
+
+        const headers = {
+            "Authorization": `Bearer ${this.apiKey}`
+        }
+        const params: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
+            method: "post",
+            muteHttpExceptions: true,
+            contentType: "application/json",
+            headers: headers,
+            payload: JSON.stringify({
+                'model': 'text-davinci-003',
+                'max_tokens': 200,
+                'temperature': 0.8,
+                'top_p': 1.0,
+                'frequency_penalty': 1.0,
+                'presence_penalty': 1.0,
+                'prompt': prompt
+            })
+        }
+
+        return JSON.parse(UrlFetchApp.fetch(url, params).getContentText());
+    }
+}
+
+export class Cell {
+    cell: string;
+    value: string;
+
+    constructor(cell: string, value: string) {
+        this.cell = cell
+        this.value = value
+    }
+}
+
+// スプレッドシートの翌日の列のテーマを取得する
+export class TopicDomain {
+    constructor() {}
+
+    getTopicFromSpreadsheet(sheetID: string, sheetName: string): Cell | null {
+        // シートの取得
+        const sheet = new Spreadsheet(sheetID, sheetName).sheet
+        // 翌日の日付
+        const nextdayString = this.getNextDate();
+        // 日付のリスト
+        const dateList = this.getDateList(sheet);
+
+        let theme = "";
+        for (let i = 0; i < dateList.length; i++) {
+            if (dateList[i][0] == nextdayString) {
+                // 隣のセルの番号にあるテーマを取得する
+                const cell = `B${i + 2}`;
+                const specificDateRange = sheet.getRange(cell);
+                theme = specificDateRange.getDisplayValue();
+                return new Cell(cell, theme)
+            }
+        }
+        return null
+    }
+
+    /**
+     * スプレッドシートから日付のリストを取得する
+     */
+    private getDateList(sheet: GoogleAppsScript.Spreadsheet.Sheet): string[][] {
+        const lastRow = sheet.getLastRow();
+        const dateRange = sheet.getRange(2, 1, lastRow - 1);
+        return dateRange.getDisplayValues();
+    }
+
+    /**
+    * 翌日の日付を yyyy/M/dd で取得する
+    */
+    private getNextDate(): string {
+        let nextday = new Date();
+        nextday.setDate(nextday.getDate() + 1);
+        nextday.setHours(0);
+        nextday.setMinutes(0);
+        nextday.setSeconds(0);
+        nextday.setMilliseconds(0);
+        const nextdayString = nextday.toLocaleDateString('ja-JA');
+        console.log(nextdayString)
+        return nextdayString
+    }
+}
+
+function main() {
+    // 翌日のテーマを取得する
+    const sheetID = PropertiesService.getScriptProperties().getProperty("SPREADSHEET_ID");
+    const sheetName = PropertiesService.getScriptProperties().getProperty("SHEET_NAME");
+    const topicDomain = new TopicDomain();
+    const tomorrowTopicCellOnSpreadsheet = topicDomain.getTopicFromSpreadsheet(sheetID, sheetName);
+    const tomorrowTopic = tomorrowTopicCellOnSpreadsheet.value;
+    let topic = tomorrowTopicCellOnSpreadsheet.value;
+
+    // 翌日のテーマがなければ、OpenAI API で生成する
+    if (tomorrowTopic == "") {
+        const key = PropertiesService.getScriptProperties().getProperty("OPEN_AI_API");
+        const openAIClient = new OpenAIClient(key);
+        const prompt = "あなたはChatbotで、陽気で話しかけやすい人です。\n以下の制約条件を厳密に守って、雑談テーマを1つ提案してください。\n制約条件\n* Chatbotは雑談テーマをできるだけ詳しく提案します。\n* 「おしゃべりマン」というbotです。\n* 「好きな〇〇」や「気になる〇〇」という形式で答えます。\n* 「おしゃべりマン」のあなただったらどう答えるかも教えてください。\n* 一人称は「私」です。\n* ジェンダー、家族、体、政治、宗教の話題は避けてください。";
+        const response = openAIClient.requestTextCompletion(prompt);
+        const generatedTopic = response.choices[0].text.replace("\n", "")
+        topic = generatedTopic
+        
+        // 書き込む
+        const sheet = new Spreadsheet(sheetID, sheetName);
+        sheet.write(tomorrowTopicCellOnSpreadsheet.cell, generatedTopic);
+    }
+    console.log(topic)
+
+    // テーマを投稿する
+    const token = PropertiesService.getScriptProperties().getProperty("SLACK_BOT_TOKEN");
+    const slackAPIClient = new SlackClient(token);
+    const channelID = PropertiesService.getScriptProperties().getProperty("SLACK_CHANNEL_ID");
+    slackAPIClient.postMessage(channelID, generateNotifyBlockMessage_(topic));
+}
+
+function generateNotifyBlockMessage_(topic: string) {
+    return {
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": ":sunny::sunny::sunny: *明日10:15から Slack で雑談会します！*:sunny::sunny::sunny:"
+                }
+            },
+            {
+                "type": "divider"
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": `*お題*\n${topic}`
+                },
+                "accessory": {
+                    "type": "image",
+                    "image_url": "https://user-images.githubusercontent.com/47489629/222876369-00291ef8-ee8d-4550-9c5f-d640c6dec87f.png",
+                    "alt_text": "alt text for image"
+                }
+            },
+            {
+                "type": "divider"
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": `参加お待ちしてます :raised_hands:（雑談テーマの確認・変更は<https://github.com/google/clasp|ここから>）`
+                }
+            }
+        ]
+    }
+}
